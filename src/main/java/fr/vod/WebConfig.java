ea94e45f7bb01fb3094d16de3cddf9f27219686e
pricilliaedou <pricillia.edou@gmail.com>
2025-09-25 18:07:37 +0200

    Subscribe and login (cors)

--- src/main/java/fr/vod/WebConfig.java
+++ src/main/java/fr/vod/WebConfig.java
@@ -17,36 +17,14 @@ public class WebConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**") // Allow all endpoints
-//	                        .allowedOrigins("http://localhost:9090") // Allow React app origin
+				registry.addMapping("/**") 
 						.allowedOrigins(allowedOrigin)
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP  methods
-						.allowedHeaders("*") // Allow all headers
-						.allowCredentials(true); // Allow credentials (cookies, etc.)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+						.allowedHeaders("*") 
+						.allowCredentials(true); 
 			}
 		};
 	}
 }
 
-//package fr.vod;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Value("${app.url}")
-//    private String allowedOrigin;
-//
-//@Override
-//public void addCorsMappings(CorsRegistry registry) {
-//
-//registry.addMapping("/**")
-//.allowedOrigins(allowedOrigin)
-//.allowedMethods("*")
-//.allowedHeaders("*")
-//.allowCredentials(true);
-//}
-//}
+
